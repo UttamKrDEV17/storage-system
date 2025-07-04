a3e965f15647290d3463cc68253dc6ddb8097d5d
uttam17 <uttamkr244@gmail.com>
2025-07-04 23:27:11 +0530

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(morgan('combined'));
+app.use(express.json());
+
+
+export default app;
---
+++ backend/src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected successfully at ${new Date().toLocaleString()} `);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
